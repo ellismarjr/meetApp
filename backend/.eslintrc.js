1f61d16ba6d1d30d3d28e989c9f8614208ea3376
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-09-12 14:21:13 -0300

    feat: File Upload

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -3,8 +3,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['airbnb-base', 'prettier'],
-  plugins: ['prettier'],
+  extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
