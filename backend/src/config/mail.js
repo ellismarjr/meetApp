8d8fb242014423f45ae14d40fdaf3e4b5d2b97ed
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-09-25 10:07:35 -0300

    Environment variable settings

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '909459a1caebe0',
-    pass: '646a4e5a8eeeb6',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe MeetApp <noreply@meetapp.com>',
