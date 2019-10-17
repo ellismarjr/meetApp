8871f4254f7fc967877c22914938ca89d1158bcc
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-17 15:01:28 -0300

    feat: Add cors on the project

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import express from 'express';
 import * as Sentry from '@sentry/node';
 import Youch from 'youch';
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 
 import routes from './routes';
@@ -24,7 +25,7 @@ class App {
 
   middlewares() {
     this.server.use(Sentry.Handlers.requestHandler());
-
+    this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
       '/files',
