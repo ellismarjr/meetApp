3df3e085ca484e57c7dcc48414326ffa5cfc258c
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-17 15:04:24 -0300

    feat: Verify if user is a provider, if not the user cannot login

--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -1,38 +1,52 @@
 import * as Yup from 'yup';
 import jwt from 'jsonwebtoken';
 
-import authConfig from '../../config/auth'
-import User from '../models/User'
+import authConfig from '../../config/auth';
+import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
     const schema = Yup.object().shape({
-      email: Yup.string().email().required(),
+      email: Yup.string()
+        .email()
+        .required(),
       password: Yup.string().required(),
     });
 
     if (!(await schema.isValid(req.body))) {
-      return res.status(400).json({ error: "Validation fails" })
+      return res.status(400).json({ error: 'Validation fails' });
     }
     const { email, password } = req.body;
 
-    const user = await User.findOne({ where: { email } })
+    const user = await User.findOne({
+      where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
+    });
 
     if (!user) {
-      return res.status(401).json({ error: "User not found" });
+      return res.status(401).json({ error: 'User not found' });
     }
 
     if (!(await user.checkPassword(password))) {
-      return res.status(401).json({ error: "Password does not match" });
+      return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
-        email
+        email,
+        provider,
+        avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
         expiresIn: authConfig.expiresIn,
@@ -41,4 +55,4 @@ class SessionController {
   }
 }
 
-export default new SessionController()
+export default new SessionController();
