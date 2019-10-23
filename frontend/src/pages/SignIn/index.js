35aff3093bf3d7295816ff8e54ccfb29d034ba33
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-23 17:29:41 -0300

    Storage user info to authentication

--- frontend/src/pages/SignIn/index.js
+++ frontend/src/pages/SignIn/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
 
 import logo from '~/assets/logo.svg';
 
@@ -13,11 +16,16 @@ const schema = Yup.object().shape({
 });
 
 export default function SignIn() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
   return (
     <>
       <img src={logo} alt="MeetApp" />
 
-      <Form schema={schema}>
+      <Form onSubmit={handleSubmit} schema={schema}>
         <Input name="email" type="email" placeholder="Seu e-mail" />
         <Input name="password" type="password" placeholder="Sua senha" />
 
--- frontend/src/routes/Route.js
+++ frontend/src/routes/Route.js
@@ -5,12 +5,14 @@ import PropTypes from 'prop-types';
 import AuthLayout from '../pages/_laytouts/auth';
 import DefaultLayout from '../pages/_laytouts/default';
 
+import { store } from '~/store';
+
 export default function RouteWrapper({
   component: Component,
   isPrivate,
   ...rest
 }) {
-  const signed = false;
+  const { signed } = store.getState().auth;
 
   if (!signed && isPrivate) {
     return <Redirect to="/" />;
