473c7c9d4cbb02ac3d2d1637e7fec368c185f780
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-24 10:16:07 -0300

    style: Header Style

---
+++ frontend/src/components/Header/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logoheader.svg';
+
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <Link to="/dashboard">
+            <img src={logo} alt="MeetApp" />
+          </Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Administrador</strong>
+              <Link to="/profile">Meu Perfil</Link>
+            </div>
+            <button type="button">Sair</button>
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
---
+++ frontend/src/components/Header/styles.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 92px;
+  background: #000;
+`;
+
+export const Content = styled.div`
+  padding: 30px;
+  max-width: 940px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  aside {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+export const Profile = styled.div`
+  display: flex;
+  margin-left: 30px;
+  padding-left: 30px;
+
+  div {
+    text-align: right;
+    margin-right: 30px;
+
+    strong {
+      display: block;
+      color: #fff;
+      font-size: 14px;
+    }
+
+    a {
+      display: block;
+      margin-top: 5px;
+      font-size: 14px;
+      color: #999;
+    }
+  }
+
+  button {
+    height: 42px;
+    width: 71px;
+    border-radius: 4px;
+    border: 0;
+    background: #d44059;
+    color: #fff;
+    font-weight: bold;
+    font-size: 16px;
+  }
+`;
