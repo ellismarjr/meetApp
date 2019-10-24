f5b64148ae5bbb5583d774a405253d7ec3f9a133
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-24 09:03:27 -0300

    feat: Login persists while refreshing page

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,8 @@
 import { persistStore } from 'redux-persist';
-
 import createSagaMiddleware from 'redux-saga';
 
 import createStore from './createStore';
-import persistedReducers from './persistReducers';
+import persistReducers from './persistReducers';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -16,7 +15,7 @@ const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
 const middlewares = [sagaMiddleware];
 
-const store = createStore(persistedReducers(rootReducer), middlewares);
+const store = createStore(persistReducers(rootReducer), middlewares);
 const persistor = persistStore(store);
 
 sagaMiddleware.run(rootSaga);
