6dd96d883516f987548247d54b82d257926e3894
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-24 16:37:50 -0300

    feat: Added signout of the application

--- frontend/src/components/Header/index.js
+++ frontend/src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+
+import { signOut } from '~/store/modules/auth/actions';
 
 import Notifications from '~/components/Notifications';
 import logo from '~/assets/logoheader.svg';
@@ -9,6 +11,12 @@ import { Container, Content, Profile } from './styles';
 
 export default function Header() {
   const profile = useSelector(state => state.user.profile);
+  const dispatch = useDispatch();
+
+  function handleSingOut() {
+    dispatch(signOut());
+  }
+
   return (
     <Container>
       <Content>
@@ -25,7 +33,9 @@ export default function Header() {
               <strong>{profile.name}</strong>
               <Link to="/profile">Meu Perfil</Link>
             </div>
-            <button type="button">Sair</button>
+            <button type="button" onClick={handleSingOut}>
+              Sair
+            </button>
           </Profile>
         </aside>
       </Content>
