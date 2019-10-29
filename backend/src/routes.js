327ff2c3bacb727441939c356f015a2afdbe0bb5
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-10-29 08:39:09 -0300

    fix: files upload on new meetup

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ import SessionController from './app/controllers/SessionController';
 import MeetupController from './app/controllers/MeetupController';
 import SubscriptionController from './app/controllers/SubscriptionController';
 import NotificationController from './app/controllers/NotificationController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -36,5 +37,7 @@ routes.delete('/subscriptions/:id', SubscriptionController.delete);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
